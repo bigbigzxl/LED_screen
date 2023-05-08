36e4dce714487db3a9d9f508fbebb79aea609215
bigbigzxl <zxloas@icloud.com>
2023-05-08 22:40:12 +0800

    menu 61%

--- src/main.cpp
+++ src/main.cpp
@@ -69,6 +69,9 @@ void click()
 void doubleclick()
 {
   // director->runWithScene(dot2d::TripleClick::create());
+  menu.set_power_state(!menu.get_power_state()); // for test.
+  Serial.println("double clicked. %d", menu.get_power_state());
+
   menu.set_button_state(Menu::ButtonType::D_CLICK);
 }
 
